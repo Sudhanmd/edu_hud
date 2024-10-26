160b40b94154e638323b11eeb224ca1c6a630dd8
sudanmd <sudanmd2002@gmail.com>
2024-10-26 16:24:37 +0530

    update enum in user and create course enitiy

---
+++ src/entity/course.entity.ts
@@ -0,0 +1,38 @@
+import {Column, Entity, JoinColumn, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn} from 'typeorm';
+import { User_entity } from './user.entity';
+
+@Entity()
+export class Course_entity{
+
+    @Column()
+    @PrimaryGeneratedColumn('uuid')
+    id : string;
+
+    @Column()
+    title :string;
+
+    @Column()
+    description : string;
+
+    @Column()
+    startDate : Date;
+
+    @Column()
+    endDate : Date;
+
+    @ManyToOne(() => User_entity,(user)=>user.id ,{onDelete:'CASCADE'}) 
+    @JoinColumn({name:'user'})
+    user: User_entity;
+
+    @Column()
+    category : string
+
+    @Column({
+        type :'enum',
+        enum : ['BEGINNER', 'INTERMEDIATE', 'ADVANCED'] })
+    level:string;
+
+
+    @OneToMany(()=> Course_entity, (course)=>course.id)
+    course : Course_entity;
+}
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Course_entity } from "./course.entity";
 
 @Entity()
 export class User_entity{
@@ -12,7 +13,9 @@ export class User_entity{
     @Column()
     password: string
 
-    @Column({type:'enum'})
+    @Column({
+        type:'enum',
+        enum : ['EDUCATOR', 'STUDENT'] })
     role: string
 
     @Column({unique:true})
@@ -23,5 +26,7 @@ export class User_entity{
 
     @Column()
     image : string
-
+    
+    @OneToMany(() => User_entity, user=>(user.id))
+    user :User_entity
 }
