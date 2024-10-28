d35f6da10b2190cc7026f89771b4b7c76c313c98
sudanmd <sudanmd2002@gmail.com>
2024-10-28 15:35:32 +0530

    updating app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root@123',
+      database: 'edu_hub',
+      autoLoadEntities: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+  }),
+  UserModule,
+  CourseModule,
+], controllers: [],
+  providers: [],
 })
 export class AppModule {}
