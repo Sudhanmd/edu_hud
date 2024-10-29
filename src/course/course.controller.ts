b2c5df2ffa4bfb1ebdb3cbf87bfb0630ea03abf2
sudanmd <sudanmd2002@gmail.com>
2024-10-29 12:57:17 +0530

    updating the route because i initially commit wrong route

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,38 +1,45 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { Course_entity} from 'src/entity/course.entity';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Course_entity } from 'src/entity/course.entity';
 import { CourseService } from './course.service';
 import { updateCourseDto } from './course.dto';
 
-
 @Controller('course')
 export class CourseController {
-    constructor(private readonly courseModel: CourseService) {}
+  constructor(private readonly courseModel: CourseService) {}
 
-     @Post('addCourse')
-     async createcourser(@Body() body:Course_entity){
-        return await this.courseModel.createcourse(body)
-     }
+  @Post('addCourse')
+  async createcourser(@Body() body: Course_entity) {
+    return await this.courseModel.createcourse(body);
+  }
 
-     @Get('getAllCourses')
-     async getAllCourse(){
-        return await this.courseModel.getAllCourse()
-     }
+  @Get('getAllCourses')
+  async getAllCourse() {
+    return await this.courseModel.getAllCourse();
+  }
 
   @Get('getCourseByCourseId/:Id')
-  async getUserById(@Param('id')id: string)
-{ return await this.courseModel.getCourseById(id)}
+  async getCourseById(@Param('Id') id: string) {
+    return await this.courseModel.getCourseById(id);
+  }
 
   @Put('updateCourseByCourseId/:courseId')
-    async updateUserById(
-      @Param('id') id: string,
-      @Body() body:updateCourseDto){
-    return await this.courseModel.updateUserById(id,body)
+  async updateCourseById(
+    @Param('courseId') id: string,
+    @Body() body: updateCourseDto,
+  ) {
+    return await this.courseModel.updateCourseById(id, body);
   }
 
   @Delete('deleteCourseByCourseId/:courseId')
-  async deleteCourse(@Param('Id') id: string) {
+  async deleteCourse(@Param('courseId') id: string) {
     return await this.courseModel.deleteCourse(id);
   }
-
-
 }
--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Course_entity } from 'src/entity/course.entity';
 import { Repository } from 'typeorm';
@@ -6,67 +10,67 @@ import { updateCourseDto } from './course.dto';
 
 @Injectable()
 export class CourseService {
-    constructor(
-        @InjectRepository(Course_entity)
-        private readonly courseRepository: Repository<Course_entity>,
-      ) {} 
-      
-//creating the course
-   async createcourse(body:updateCourseDto) {
-    try {
-    const createuser = await this.courseRepository.save(body)
-    return{success:true, message:createuser}
-    } 
-    catch (error) {
-        throw new BadRequestException(error.message || error);
-    }     
-}
+  constructor(
+    @InjectRepository(Course_entity)
+    private readonly courseRepository: Repository<Course_entity>,
+  ) {}
 
-//get all users
-   async getAllCourse() {
+  //creating the course
+  async createcourse(body: updateCourseDto) {
     try {
-        const getcourse = await this.courseRepository.find()
-        if(!getcourse) throw new NotFoundException(`No course Found`)
-        return getcourse;
+      const createuser = await this.courseRepository.save(body);
+      return { success: true, message: createuser };
     } catch (error) {
-        throw new BadRequestException(error.message || error)
+      throw new BadRequestException(error.message || error);
     }
+  }
+
+  //get all users
+  async getAllCourse() {
+    try {
+      const getcourse = await this.courseRepository.find();
+      if (!getcourse) throw new NotFoundException(`No course Found`);
+      return getcourse;
+    } catch (error) {
+      throw new BadRequestException(error.message || error);
+    }
+  }
+
+  //get course by courseId
+  async getCourseById(givenid: string) {
+    try {
+      const getcourse = await this.courseRepository.findOneBy({ id: givenid });
+      if (!getcourse)
+        throw new NotFoundException(`given ${givenid} is not found`);
+      return getcourse;
+    } catch (error) {
+      throw new BadRequestException(error.message || error);
+    }
+  }
+
+  async updateCourseById(id: string, body: updateCourseDto) {
+    try {
+      const checkCourseID = await this.courseRepository.findOne({
+        where: { id: id },
+      });
+      if (!checkCourseID)
+        throw new NotFoundException(`given ${id} is not found`);
+      const updateCouseId = await this.courseRepository.update(id, body);
+      return { success: true, message: updateCouseId };
+    } catch (error) {
+      throw new BadRequestException(error.message || error);
+    }
+  }
+
+  async deleteCourse(id: string) {
+    try {
+      const deleteCourse = await this.courseRepository.delete(id);
+      if (deleteCourse.affected === 0) {
+        throw new NotFoundException(`courseId ${id} is not found`);
+      }
+      return { message: `${id} is deleted successfully` };
+    } catch (error) {
+      throw new BadRequestException(error.message || error);
+    }
+  }
 }
-
-//get course by courseId
-  async  getCourseById(id: string) {
-        try {
-            const getcourse = await this.courseRepository.find({where:{id:id}})
-        if(!getcourse) throw new NotFoundException(`given ${id} is not found`)
-        return getcourse;
-        } 
-        catch (error) {
-            throw new BadRequestException(error.message || error);
-        }
-    }
-
-async updateUserById(id: string, body: updateCourseDto) {
-        try {
-         const checkCourseID = await this.courseRepository.findOne({where:{id:id}})
-        if(!checkCourseID) throw new NotFoundException(`given ${id} is not found`)
-         const updateCouseId = await this.courseRepository.update(id,body)
-        return {success:true,message:updateCouseId};
-        } 
-        catch (error) {
-            throw new BadRequestException(error.message || error);
-        }
-    }
-
-    async deleteCourse(id:string){
-        try {
-            const deleteCourse = await this.courseRepository.delete(id)
-            if(deleteCourse.affected === 0){
-              throw new NotFoundException (`courseId ${id} is not found`);   
-            }
-          return {message:`${id} is deleted successfully`};
-        } catch (error) {
-            throw new BadRequestException(error.message || error)}
-          }
-
-}
- 
