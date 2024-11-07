47e4d2ae6e79089875c2b0f0fd29bbf53b207332
sudanmd <sudanmd2002@gmail.com>
2024-11-07 10:37:19 +0530

    uploading the files to main branch

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { Repository } from 'typeorm';
+import { User_entity } from 'src/entity/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const mockservice = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+};
 
 describe('UserService', () => {
   let service: UserService;
+  let userRepository: Repository<User_entity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserService),
+          useValue: mockservice,
+        },
+      ],
     }).compile();
-
     service = module.get<UserService>(UserService);
+    userRepository = module.get<Repository<User_entity>>(
+      getRepositoryToken(User_entity),
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-});
+  it('should create a new user', async () => {
+    jest.spyOn(userRepository, 'findOne').mockResolvedValueOnce(null);
+    jest.spyOn(userRepository, 'save').mockResolvedValueOnce(User_entity);
+});});
